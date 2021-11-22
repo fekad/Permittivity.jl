7b7a3c730c0d9fd5400d7b5c26b13c3308b1ae56
fekad <adam@fekete.co.uk>
2021-11-22 14:18:33 +0100

    missing definitions

--- src/composite.jl
+++ src/composite.jl
@@ -1,17 +1,52 @@
-# @doc raw"""
-# ## Arbitrarily-accurate Models
-#
-# The arbitrarily-accurate (albeit, perhaps unphysical) fits to experimental optical data can be obtained.
-#
-# - multi-pole Debye model with static conductivity
-#   $$\varepsilon (\omega) = \varepsilon_\infty  + \sum \limits_i^N \frac{\Delta\varepsilon_i}{1 + j \omega \tau_i} + \frac{\sigma_s}{j \omega \varepsilon_0}$$
-#   where
-#   - $\sigma_s$ the static conductivity
-#   - $\Delta\varepsilon_i$ is the change in permittivity due to the ith dispersion
-# - Drude model in combination with multiple Lorentz oscillator poles
-#   $$\varepsilon (\omega) = \varepsilon_\infty  + \sum \limits_N \frac{\Delta\varepsilon_p \omega_p^2}{\omega_p^2 - \omega(\omega + j 2\delta)}$$
-# - Interpolated tabulated data
-#
-# """
+@doc raw"""
+    MultiPoleDebyeModel(eps_inf, sigma, eps, tau)
 
-# decomposition
+The multi-pole Debye model with static conductivity:
+```math
+\varepsilon (\omega) = \varepsilon_\infty  + \sum \limits_i^N \frac{\Delta\varepsilon_i}{1 + j \omega \tau_i} + \frac{\sigma_s}{j \omega \varepsilon_0}
+```
+where
+- $\sigma_s$ the static conductivity
+- $\Delta\varepsilon_i$ is the change in permittivity due to the ith dispersion
+
+"""
+struct MultiPoleDebyeModel{T<:AbstractFloat} <: Composite
+    eps_inf::T
+    sigma::T
+    eps::Vector{T}
+    tau::Vector{T}
+end
+
+function (m::MultiPoleDebyeModel)(omega)
+    poles = sum(1:lentgh(m.eps)) do i
+        m.eps[i] / (1 + (im * omega * m.tau[i]))
+    end
+    m.eps_inf + poles + m.sigma / (im *  omega * eps_0)
+end
+
+
+
+@doc raw"""
+    MultiLorentzPoleModel(eps_inf, eps, omega_p, delta)
+
+The Drude model in combination with multiple Lorentz oscillator poles:
+```math
+\varepsilon (\omega) = \varepsilon_\infty  + \sum \limits_N \frac{\Delta\varepsilon \omega_p^2}{\omega_p^2 - \omega(\omega + j 2\delta)}
+```
+"""
+struct MultiLorentzPoleModel{T<:AbstractFloat} <: Composite
+    eps_inf::T
+    eps::Vector{T}
+    omega_p::Vector{T}
+    delta::Vector{T}
+end
+
+function (m::MultiLorentzPoleModel)(omega)
+    poles = sum(1:lentgh(m.eps)) do i
+        m.eps[i] * m.omega_p[i]^2 / (m.omega_p[i]^2 - omega * (omega + im * 2 * m.delta[i]))
+    end
+    m.eps_inf + poles
+end
+
+
+
--- src/tabulated.jl
+++ src/tabulated.jl
@@ -1,25 +1,27 @@
+@doc raw"""
+    PermittivityTable(omega, eps)
 
-# # Interpolated frequency dependent permitivirty
-# struct PermittivityTable <: TablePermittivity
-#     lambda::Vector{Float64}
-#     eps::Vector{ComplexF64}
-#     itp_real::Spline1D
-#     itp_imag::Spline1D
-#     function PermittivityTable(lambda::Vector{Float64}, eps::Vector{ComplexF64})
-#
-#         inds = sortperm(lambda)
-#         # itp_real = interpolate((lambda[inds],), real(eps[inds]), Gridded(Linear()))
-#         # itp_imag = interpolate((lambda[inds],), imag(eps[inds]), Gridded(Linear()))
-#
-#         itp_real = Spline1D(lambda[inds], real(eps[inds]))
-#         itp_imag = Spline1D(lambda[inds], imag(eps[inds]))
-#
-#         new(lambda, eps, itp_real, itp_imag)
-#     end
-# end
-#
-# permittivity(e::PermittivityTable, lambda) = e.itp_real(lambda) + e.itp_imag(lambda) * im
-#
-# # TODO: proper broadcasting
-# reflective_index(e::AbstractPermittivity, lambda) = sqrt(permittivity(e, lambda))
-#
+Interpolate values between tabulated data points.
+"""
+struct PermittivityTable <: Tabulated
+    omega::Vector{Float64}
+    eps::Vector{ComplexF64}
+    _itp_real::Spline1D
+    _itp_imag::Spline1D
+
+    function PermittivityTable(omega::Vector{Float64}, eps::Vector{ComplexF64})
+
+        inds = sortperm(omega)
+
+        itp_real = Spline1D(omega[inds], real(eps[inds]))
+        itp_imag = Spline1D(omega[inds], imag(eps[inds]))
+
+        # itp_real = interpolate((omega[inds],), real(eps[inds]), Gridded(Linear()))
+        # itp_imag = interpolate((omega[inds],), imag(eps[inds]), Gridded(Linear()))
+
+        new(omega, eps, itp_real, itp_imag)
+    end
+end
+
+(e::PermittivityTable)(omega) = e._itp_real(omega) + e._itp_imag(omega) * im
+
